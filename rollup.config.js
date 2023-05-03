df415f04394ff1f6adb114102e4ef5836be81d95
Andrew Punnett <andrew.punnett@gmail.com>
2023-05-04 11:41:28 +1200

    Configure Rollup.

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+// rollup.config.js
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/bundle.cjs",
+      format: "cjs",
+    },
+    {
+      file: "dist/bundle.js",
+      format: "es",
+    },
+  ],
+};
