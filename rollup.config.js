9e400bb3ec2930025348ce824a4b043936603abe
Andrew Punnett <andrew.punnett@gmail.com>
2023-07-10 18:13:54 +1200

    Make the build process Typescript aware.

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
 export default {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: [
     {
       file: "dist/bundle.cjs",
@@ -16,7 +18,11 @@ export default {
       format: "es",
     },
   ],
-  plugins: [commonjs(), babel({ babelHelpers: "runtime" }), resolve()],
+  plugins: [
+    commonjs(),
+    babel({ extensions, babelHelpers: "runtime" }),
+    resolve({ extensions }),
+  ],
   external: [
     /@babel\/runtime/,
     "react",
