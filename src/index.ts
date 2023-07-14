01f87b17a195756eade08431a137e13f193ff440
Andrew Punnett <andrew.punnett@gmail.com>
2023-07-14 14:15:46 +1200

    Export the default numerical simulator from the package.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { NetworkGraph } from "./NetworkGraph";
 export { DynamicNetworkGraph } from "./DynamicNetworkGraph";
+export { DefaultVertexElement } from "./DefaultVertexElement";
+export { VertexElementProps } from "./NetworkGraph";
+export { updateVerticesPositionsImp } from "./defaultNumericalSimulation";
