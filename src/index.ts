a8e214545e1fd0489c6c07d8fa7740a5c6d19717
Andrew Punnett <andrew.punnett@gmail.com>
2023-07-28 14:20:33 +1200

    Cleanup the package exports.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-export { NetworkGraph } from "./NetworkGraph";
-export { DynamicNetworkGraph } from "./DynamicNetworkGraph";
-export { DefaultVertexComponent } from "./components/DefaultVertexComponent";
-export { VertexComponentProps } from "./NetworkGraph";
-export { defaultUpdateVerticesPositions } from "./utils/defaultNumericalSimulation";
+export {
+  NetworkGraph,
+  VertexSpecification,
+  Position,
+  VertexComponentProps,
+  VertexWrapperProps,
+  EdgeSpecification,
+  EdgeComponentProps,
+  NetworkGraphProps,
+} from "./NetworkGraph";
+
+export {
+  DynamicNetworkGraph,
+  VertexPosition,
+  UpdateVerticesPositions,
+  DynamicNetworkGraphProps,
+} from "./DynamicNetworkGraph";
